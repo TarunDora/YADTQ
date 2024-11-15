ec74ec7a83ce8dd546addc8feaf8ff47f2efd861
Srihari Krishna <113294358+Srihari-Krishna@users.noreply.github.com>
2024-11-15 23:40:28 +0530

    Added new functions
    Used new functions instead of add and sub. Implemented task status checking by client

--- client.py
+++ client.py
@@ -2,6 +2,7 @@ from yadtq import YADTQ
 import time
 import sys
 import json
+import redis
 
 def check_heartbeat(queue):
     status = queue.check_worker_health()
@@ -12,13 +13,35 @@ def check_heartbeat(queue):
 def main():
     queue = YADTQ(kafka_server='localhost:9092', redis_host='localhost', redis_port=6379)
     
-    if "-h" in sys.argv:
+    if len(sys.argv) > 5 or ("-s" in sys.argv and "-h" in sys.argv):
+        print(len(sys.argv))
+        print("Invalid syntax")
+        exit()
+
+    if sys.argv[-1] == "-h":
         check_heartbeat(queue)
         print("Goodbye!")
         exit()
+    
+    if sys.argv[-2] == "-s":
+        task_key = sys.argv[-1]
+        print(queue.get_task_status(task_key))
+        exit()
+
+    if sys.argv[-1] == "--help":
+        print("Commands are:")
+        print("si [P(₹),R(%),T(yrs)]  -> calculates simple interest")
+        print("ci [P(₹),R(%),T(yrs)]  -> calculates compound interest")
+        print("emi [P(₹),R(%),T(yrs)]  -> calculates simple interest")
+        print("-s [task_id]  -> check task status by ID")
+        print("-h            -> check worker health")
+        exit()
 
     comm = sys.argv[-2]
-    params = json.loads(sys.argv[-1])
+    #print(comm)
+    #print(sys.argv[-1],type(sys.argv[-1]))
+    params = json.loads((sys.argv[-1]))
+    #params = sys.argv[-1]
 
     # Send a task
     task_id = queue.send_task(comm, params)
